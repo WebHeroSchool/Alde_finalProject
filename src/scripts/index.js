5ed1194e46c94889d48ea3c658ccae4879d744f3
Aleksey Denisov <alde.owl@gmail.com>
2019-01-10 23:16:59 +0700

    сделал половину адаптивки примерно

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -18,38 +18,64 @@
 
         desc.innerHTML = descrip;
 
-        wrap.appendChild(title, desc);
+        wrap.appendChild(title);
+        wrap.appendChild(desc);
         target.appendChild(wrap);
     }
-
-    fetch(`https://api.github.com/users/${name}/repos`)
-        .then(res => res.json())
-        .then(json => {
-            const formatedResult = [];
-
-            json.map(item => {
-                const {
-                    name,
-                    html_url,
-                    description
-                }= item;
-
-                formatedResult.push({
-                    name,
-                    html_url,
-                    description
+    function getRepos (name){
+        fetch(`https://api.github.com/users/${name}/repos`)
+            .then(res => res.json())
+            .then(json => {
+                const formatedResult = [];
+    
+                json.map(item => {
+                    const {
+                        name,
+                        html_url,
+                        description
+                    }= item;
+    
+                    formatedResult.push({
+                        name,
+                        html_url,
+                        description
+                    });
+                });
+    
+                console.log(formatedResult);
+                return formatedResult;
+            })
+            .then(result => { 
+                result.forEach(item => {
+                    renderRepos(item.name, item.html_url, item.description);
+    
                 });
             });
-
-            console.log(formatedResult);
-            return formatedResult;
-        })
-        .then(result => { 
-            result.forEach(item => {
-                renderRepos(item.name, item.html_url, item.description);
-
-            });
+    }
+    function scrollToItem (){
+        let worksBtn = document.querySelector('#myworks');
+        let worksWind = document.querySelector('.works');
+        
+        worksBtn.addEventListener('click', () => {
+            worksWind.scrollIntoView({behavior: 'smooth'});
         });
+
+        let skillsBtn = document.querySelector('#skills');
+        let skillsWind = document.querySelector('.skills');
+        
+        skillsBtn.addEventListener('click', () => {
+            skillsWind.scrollIntoView({behavior: 'smooth'});
+        });
+
+        let gitBtn = document.querySelector('#git');
+        let gitWind = document.querySelector('.git');
+        
+        gitBtn.addEventListener('click', () => {
+            gitWind.scrollIntoView({behavior: 'smooth'});
+        });
+    }
+    getRepos(name);
+    scrollToItem();
 })();
 
 
